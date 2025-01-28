f5c56f91ddaaca9e170bc9c54f377e932d631fb9
Kevin Boissonneault <1865974@cegeplimoilou.ca>
2025-01-27 21:24:08 -0500

    Category CRUD completed

---
+++ Repository/CategoryRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YumBlazor.Data;
+using YumBlazor.Repository.IRepository;
+
+namespace YumBlazor.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await _db.Category.AddAsync(category);
+            await _db.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var obj = await _db.Category.FirstOrDefaultAsync(u => u.Id == id);
+            if (obj != null)
+            {
+                _db.Category.Remove(obj);
+                return (await _db.SaveChangesAsync()) > 0;
+            }
+            return false;
+        }
+
+        public async Task<Category> GetAsync(int id)
+        {
+            var obj = await _db.Category.FirstOrDefaultAsync(u => u.Id == id);
+            if (obj == null)
+            {
+                return new Category();
+            }
+            return obj;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _db.Category.ToListAsync();
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            var objFromDb = await _db.Category.FirstOrDefaultAsync(u => u.Id == category.Id);
+            if (objFromDb is not null)
+            {
+                objFromDb.Name = category.Name;
+                _db.Category.Update(objFromDb);
+                await _db.SaveChangesAsync();
+                return objFromDb;
+            }
+            return category;
+        }
+    }
+}
---
+++ Services/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace YumBlazor.Services.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrtSuccess(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async Task ToastrtError(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
